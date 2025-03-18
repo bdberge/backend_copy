04936148cb201dc70a1ef425fa7ca2b370de45ef
Brandon Berge <bdberge@gmail.com>
2025-03-18 16:36:59 -0700

    add all

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+});
+
+// a route
+const axios = require("axios");
+app.get("/weather", (req, res) => {
+  const city = req.query.city;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=2601acd1ff00f2d48f651446e64e4fe6`;
+  axios
+    .get(url)
+    .then((response) => {
+      const weatherData = {
+        temperature: response.data.main.temp,
+        description: response.data.weather[0].description,
+        icon: response.data.weather[0].icon,
+      };
+      res.json(weatherData);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "An error occurred" });
+    });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log('Server running on port ${PORT}');
+});
